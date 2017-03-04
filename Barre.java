5380bbe71b72ca13b2530ccbd473e5b3a9f7babc
N0fix <neolipop2@hotmail.fr>
2017-03-04 11:24:05 +0100

    Uploadé la classe Barre

--- Barre.java
+++ Barre.java
@@ -8,9 +8,9 @@ public class Barre extends GameObject {
 	private BufferedImage barre;
 
 	public Barre(int x,int y) {
-		super(x, y);
+		super(0, 0);
 		barre = Niveau.loadBufferedImage(".."+File.separator+"image"+File.separator+"Barre.png");
-		this.positionX = (int) Math.floor(Math.random() * 250);
+		this.positionX = 280 + (int)(Math.random() * ((400 - 280) + 1));
 		this.positionY = (int) Math.floor(Math.random() * 340)+220;
 		this.largeur = getPositionX()+600;
 		this.hauteur = getPositionY()+80;
